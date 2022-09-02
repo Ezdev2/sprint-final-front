ba7ffdd86a228d9452bdaab64b3f6b22e6fa238c
Ezra <fansoaezra@gmail.com>
2022-09-02 10:47:50 +0300

    creating project

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "antd/dist/antd.css";
+import "./index.scss";
+
+console.log("tonga ato ne za e");
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
